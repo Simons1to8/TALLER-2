9c7769c76d5955d657aa34b01d50d74d3bbc5c87
Simons1to8 <simon.arangov@upb.edu.co>
2026-08-06 14:53:40 -0500

    mi segundo commit :)
    se subió a github el proyecto

---
+++ Assets/SCRIPT/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float direction;
+    public float speed;
+    public Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    rb.linearVelocity=new Vector2(direction*speed, rb.linearVelocityY);    
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+     direction= context.ReadValue<Vector2>().x;   
+    }
+}
