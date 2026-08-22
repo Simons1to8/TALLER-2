6072f3dcb5f598e19412cac5af97b6424d6d8cfe
Ale <simon.arangov@upb.edu.co>
2026-08-22 03:54:13 -0500

    Actualizacion
    me desatrase de las clases anteriores , solo me falta una para que la suba

--- Assets/SCRIPT/PlayerController.cs
+++ Assets/SCRIPT/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,24 +8,58 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
 
-    public float jumpForce;
-    public Transform groundCheck;
+    [SerializeField] private float jumpForce;
+    public bool canJump;
 
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private float GroundCheckRadius;
+    [SerializeField] private LayerMask GroundLayer;
+
+    public GameObject Key;
+    public GameObject Door;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    { 
     }
 
     // Update is called once per frame
     void Update()
     {
-    rb.linearVelocity=new Vector2(direction*speed, rb.linearVelocityY);    
+        canJump = Physics2D.OverlapCircle(GroundCheck.position,GroundCheckRadius,GroundLayer);
+
+
+      rb.linearVelocity=new Vector2(direction*speed, rb.linearVelocityY);
+      //rb.AddForceX(direction * speed);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-     direction= context.ReadValue<Vector2>().x;   
+        direction = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && canJump) 
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Key")){
+
+            collision.gameObject.SetActive(false);
+            Door.SetActive(false);
+        }
+
+        if (collision.gameObject.CompareTag("Copa")){
+            collision.gameObject.SetActive(false);
+          
+
+        }
     }
 }
