ce3d821a1d06b007eb61604a9ad66d26c9324122
Simonsito <simon.arangov@upb.edu.co>
2026-08-29 02:20:30 -0500

    K

--- Assets/SCRIPT/PlayerController.cs
+++ Assets/SCRIPT/PlayerController.cs
@@ -32,8 +32,8 @@ public class PlayerController : MonoBehaviour
         canJump = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, GroundLayer);
 
 
-        //rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocityY);
-        rb.AddForceX(direction * speed);
+        rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocityY);
+        //rb.AddForceX(direction * speed);
 
         
         if (!isFacingRight && direction > 0f)
@@ -68,6 +68,15 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            collision.gameObject.SetActive(false);
+            Door.SetActive(false);
+        }
+
+
+
+
         if (collision.gameObject.CompareTag("Key")){
 
             collision.gameObject.SetActive(false);
